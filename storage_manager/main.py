f8c63fd836a90b2f2bc664e26ddce8076b3878fe
UnboundSB <sidbhatt85@gmail.com>
2026-02-14 01:09:37 +0530

    proceeded further twards the storage_manager main.py to create a robust system for file management

--- storage_manager/main.py
+++ storage_manager/main.py
@@ -1,3 +1,20 @@
 import page
 import dll_loader
 import pickle
+import os
+import multiprocessing
+import threading
+from abc import ABC
+from Queryparser.breaker import *
+
+class StorageManager(ABC):
+    num_operations=0
+    def __init__(self):
+        self.dir=os.path()
+    
+    @staticmethod
+    def _start_operation(self):
+        self.num_operations+=1
+    @staticmethod 
+    def _end_operation(self):
+        self.num_operations-=1
