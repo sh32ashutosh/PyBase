83f54e8019a94f44c3938b2d257aa466ce6f89a5
UnboundSB <sidbhatt85@gmail.com>
2026-02-15 02:04:13 +0530

    CONFUSION:Implemented main.py storagemgr but failed

--- storage_manager/main.py
+++ storage_manager/main.py
@@ -5,16 +5,23 @@ import os
 import multiprocessing
 import threading
 from abc import ABC
-from Queryparser.breaker import *
+
 
 class StorageManager(ABC):
     num_operations=0
     def __init__(self):
-        self.dir=os.path()
+        self.dir=None
+        self.num_operations=0
     
     @staticmethod
     def _start_operation(self):
+
         self.num_operations+=1
     @staticmethod 
     def _end_operation(self):
         self.num_operations-=1
+
+if __name__=="__main__":
+    sm=StorageManager()
+    sm._start_operation()
+    print(sm.num_operations)
