c6f22ed92054caabde453363c493db171b941ec3
UnboundSB <sidbhatt85@gmail.com>
2026-02-15 02:12:53 +0530

    CONFUSION:Implemented main.py storagemgr but failed

--- storage_manager/lib/rawCPP/tree.hpp
+++ storage_manager/lib/rawCPP/tree.hpp
@@ -3,3 +3,10 @@
 #include <vector>
 #include <cstdlib>
 #include<cstdio>
+#include<mutex>
+
+#ifdef _WIN32
+    #define DLL_EXPORT __declspec(dllexport)
+#else
+    #define DLL_EXPORT __attribute__((visibility("default")))
+#endif
